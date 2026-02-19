47b41bea3e88255ff8e63ebdc39e7adb4be6275a
filipeAugusto99 <filipecordeiro213@gmail.com>
2026-02-19 16:13:46 -0300

    improv and Unescaping HTML Entities

--- data.py
+++ data.py
@@ -1,11 +1,20 @@
 import requests
 
+
+parameters = {
+    "amount": 10,
+    "type": "boolean"
+}
+
+
 question_data = []
 
+
 # getting response API
-response = requests.get(url="https://opentdb.com/api.php?amount=10&type=boolean")
+response = requests.get(url="https://opentdb.com/api.php?amount=10&type=boolean", params=parameters)
 response.raise_for_status()
 data = response.json()
+
 
 for item in data["results"]:
     question_data.append(item)
